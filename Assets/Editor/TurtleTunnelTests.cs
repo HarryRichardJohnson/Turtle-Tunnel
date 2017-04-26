f869571f2aa128d2abf16b70383bce9e64a2b9d3
Casey Glover <casey.n.glover@gmail.com>
2017-04-27 08:20:49 +1200

    Basic Test Set up

---
+++ Assets/Editor/TurtleTunnelTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class TurtleTunnelTests {
+
+	[Test]
+	public void FailingTest(){
+		Assert.AreEqual(1,2);
+	}
+
+}
