f6f55c8b0b8f951e72eedae650af51b1e625d48b
Cyril Sebastian <cyrilsebastian96@gmail.com>
2017-05-02 02:35:22 +1200

    Added pause button.

--- Assets/Editor/NewEditModeTest.cs
+++ Assets/Editor/NewEditModeTest.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
@@ -117,12 +116,12 @@ public class NewEditModeTest {
 
 	// A UnityTest behaves like a coroutine in PlayMode
 	// and allows you to yield null to skip a frame in EditMode
-	[UnityTest]
+/*	[UnityTest]
 	public IEnumerator NewEditModeTestWithEnumeratorPasses() {
 		// Use the Assert class to test conditions.
 		// yield to skip a frame
 		yield return null;
-	}
+	}*/
 
 
 }
--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -4,9 +4,20 @@ using UnityEngine.UI;
 public class HUD : MonoBehaviour {
 
 	public Text distanceLabel, velocityLabel;
+	public bool isPaused = false;
 
 	public void SetValues (float distanceTraveled, float velocity) {
 		distanceLabel.text = ((int)(distanceTraveled * 10f)).ToString();
 		velocityLabel.text = ((int)(velocity * 10f)).ToString();
+	}
+
+	public void PauseGame(){
+		if (isPaused == false) {
+			Time.timeScale = 0;
+			isPaused = true;
+		} else {
+			Time.timeScale = 1;
+			isPaused = false;
+		}
 	}
 }
