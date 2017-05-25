b4346d8eeb2a357c6170225c4dcf2ce494208a69
Ghost11696 <jmdillon166@gmail.com>
2017-05-26 10:58:22 +1200

    further shop changes

--- Assets/Scripts/SaveLoad.cs
+++ Assets/Scripts/SaveLoad.cs
@@ -2,11 +2,14 @@
 using UnityEngine;
 using System.Collections;
 
-public class SaveLoad : MonoBehaviour{
-	public static void save() {
+public class SaveLoad : MonoBehaviour
+{
+	public static void save() 
+	{
 		PlayerPrefs.SetInt("Score", Player.coinTotal);
 	}
-	public static int load() {
+	public static int load() 
+	{
 		return PlayerPrefs.GetInt("Score");
 	}
 }
