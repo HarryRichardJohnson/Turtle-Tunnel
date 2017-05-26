0dc4f3144208f96fc2771f52bab9cf59a4cf949d
Ghost11696 <jmdillon166@gmail.com>
2017-05-26 16:03:30 +1200

    implementing skin changes and shop

---
+++ Assets/ShowSkin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowSkin : MonoBehaviour 
+{
+
+	public GameObject[] turtleSkin;
+	public GameObject currentSkin;
+	public int skinUnlock;
+
+	void Start(){
+		checkSkinUnlock ();
+	}
+
+	private void checkSkinUnlock(){
+
+		switch (skinUnlock) {
+		case 0:
+			currentSkin = turtleSkin [0];
+			currentSkin.SetActive (true);
+			//Instantiate (turtleSkin [0]);
+			break;
+		case 1: 
+			currentSkin = turtleSkin [1];
+			currentSkin.SetActive (true);
+			break;
+		default:
+			currentSkin.SetActive (true);
+			break;
+		}
+	}
+}
